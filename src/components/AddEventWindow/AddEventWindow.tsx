68b0826520031ad6f171a4cbcbd889521c3aae9e
Anton <kramarenko4362@gmail.com>
2022-09-11 14:14:51 +0300

    change form buttons design

--- src/components/AddEventWindow/AddEventWindow.tsx
+++ src/components/AddEventWindow/AddEventWindow.tsx
@@ -8,6 +8,8 @@ import { addEvent, removeEvent, updateEvent } from '../../store/events';
 import { isVisibleModal } from '../../store/modalWindow';
 import { IEvent } from '../../types/types';
 import './AddEventWindow.scss';
+import { IoTrashOutline } from 'react-icons/io5';
+
 
 export const AddEventWindow = () => {
 	const navigate = useNavigate();
@@ -19,8 +21,6 @@ export const AddEventWindow = () => {
 	const id = pathname.replace('/calendar/', '').replace('/calendar', '');
 	const curentDate = dayInString(new Date().getTime());
 	const createAt = new Date(parseInt(id)).toLocaleString();
-
-	
 
 	useEffect(() => {
 		setValue('data', `${ curentDate.year }-${ curentDate.month }-${ curentDate.day }`);
@@ -78,7 +78,7 @@ export const AddEventWindow = () => {
 	return (
 		<div className='addEventWindow' onClick={e => e.stopPropagation()}>
 			<h2 className='addEventWindow__title'>{id.length ? 'Edit idea item'  :  'Add new idea item'}</h2>
-			{id.length && createAt && <h5 className='addEventWindow__subtitle'>Created at: {createAt}</h5>}
+			{id.length>0 && createAt && <h5 className='addEventWindow__subtitle'>Created at: {createAt}</h5>}
 
 			<form onSubmit={handleSubmit(onSubmit)} className='addEventWindow__form'>
 				<input  {...register('title', { required: true })} placeholder='Title goes here' className='addEventWindow__input input-title' />
@@ -98,8 +98,8 @@ export const AddEventWindow = () => {
 					</div>
 				</div> 
 				<div className='addEventWindow__btns'>
-					<button type='submit' > {id.length ? 'Update' : 'Save'}</button>
-					<button type='reset' onClick={handleReject}>{id.length ? 'Delete' : 'Reject'}</button>
+					{id.length>0 && <button type='reset' className='addEventWindow__btns-delete' onClick={handleReject}> <IoTrashOutline/> </button>}
+					<button type='submit' className='addEventWindow__btns-submit'> {id.length ? 'Update' : 'Save'}</button>
 				</div>
 			</form>
 		</div>
